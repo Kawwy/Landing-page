613bc727a39252dc25d751a56e699955bab13b37
Kawwy <kawinmathivanan.u@gmail.com>
2022-11-09 20:03:43 +0530

    Added successfully

---
+++ script.js
@@ -0,0 +1,7 @@
+const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+
+tl.to(".text", {y: "0%", duration: 1.25, stagger: 0.25}); //stagger was used to give time difference for nextLine
+tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
+tl.to(".intro", {y: "-100%", duration: 1.5}, "-=1.5"); // Here "-=1.5" is used to start the effect before 1.5secs
+tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5});
+tl.fromTo(".big-text", {opacity: 0}, {opacity: 0.5, duration: 1, delay: 0.5});
