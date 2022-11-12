60f7ce29b1d4084ed19acbfd6893af291b604195
Kawwy <kawinmathivanan.u@gmail.com>
2022-11-12 23:23:07 +0530

    Changes bg and added sticky nav bar (pending)

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
 tl.to(".intro", {y: "-100%", duration: 1.5}, "-=1.5"); // Here "-=1.5" is used to start the effect before 1.5secs
 tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5});
 tl.fromTo(".big-text", {opacity: 0}, {opacity: 0.5, duration: 1, delay: 0.5});
+
+window.addEventListener("scroll", function() {
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("active", window.screenY > 0);
+});
