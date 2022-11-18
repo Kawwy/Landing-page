ee79222091b58a04444ca5bc920301e94c800dd8
keerthanaaarthanari <keerthanaaarthanari@gmail.com>
2022-11-18 11:25:43 +0530

    Added navbar

--- Nav bar/script.js
+++ Nav bar/script.js
@@ -4,10 +4,10 @@ tl.to(".text", {y: "0%", duration: 1.25, stagger: 0.25}); //stagger was used to 
 tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
 tl.to(".intro", {y: "-100%", duration: 1.5}, "-=1.5"); // Here "-=1.5" is used to start the effect before 1.5secs
 tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1.5});
-tl.fromTo(".big-text", {opacity: 0}, {opacity: 0.5, duration: 1, delay: 0.5});
+// tl.fromTo(".big-text", {opacity: 0}, {opacity: 0.5, duration: 1, delay: 0.5});
 
-const navbar = document.getElementById("navbar");
-const menu = document.getElementById("nav-links");
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
 
 window.onscroll = function()
 {
